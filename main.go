ce8ff500a6b3d4242a8145c41bd229b47ed43b10
Grafstain <grafstain@gmail.com>
2022-02-21 17:25:47 +0300

    refactor Hello()

--- main.go
+++ main.go
@@ -14,14 +14,19 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	return
 }
 
 func main() {
