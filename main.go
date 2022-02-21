2312bbc2ce177267b716d984bf7905c4972a1848
Grafstain <grafstain@gmail.com>
2022-02-21 14:41:26 +0300

    add french and switch

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanish            = "Spanish"
+	french             = "French"
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	prefix := englishHelloPrefix
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+	return prefix + name
 }
 
 func main() {
 
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
