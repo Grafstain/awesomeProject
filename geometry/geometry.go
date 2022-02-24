3e230f1d8109cc2e7794f723a9f1a1a934250ffc
Grafstain <grafstain@gmail.com>
2022-02-24 21:28:23 +0300

    interfaces refactor for geometry

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -2,12 +2,17 @@ package geometry
 
 import "math"
 
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func (r Rectangle) Square() (P float64) {
+func (r Rectangle) Area() (P float64) {
 	return r.width * r.height
 }
 func (r Rectangle) Perimeter() (P float64) {
@@ -18,7 +23,7 @@ type Circle struct {
 	radius float64
 }
 
-func (c Circle) Square() (P float64) {
+func (c Circle) Area() (P float64) {
 	return math.Pi * c.radius * c.radius
 }
 func (c Circle) Perimeter() (P float64) {
--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -3,36 +3,32 @@ package geometry
 import "testing"
 
 func TestGeometry(t *testing.T) {
-	t.Run("Test perimeter", func(t *testing.T) {
-		r := Rectangle{10, 20}
-		got := r.Perimeter()
-		want := 60.0
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-	t.Run("Test square", func(t *testing.T) {
-		r := Rectangle{10, 20}
-		got := r.Square()
-		want := 200.0
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-	t.Run("Test square circle", func(t *testing.T) {
-		r := Circle{10}
-		got := r.Square()
-		want := 314.1592653589793
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
-	})
-	t.Run("Test perimeter circle", func(t *testing.T) {
-		r := Circle{10}
-		got := r.Perimeter()
-		want := 62.83185307179586
+	}
+
+	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Perimeter()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+
+	}
+	t.Run("Test rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10, 20}
+		checkPerimeter(t, rectangle, 60.0)
+		checkArea(t, rectangle, 200.0)
 	})
+
+	t.Run("Test circle", func(t *testing.T) {
+		rectangle := Circle{10}
+		checkPerimeter(t, rectangle, 62.83185307179586)
+		checkArea(t, rectangle, 314.1592653589793)
+	})
+
 }
